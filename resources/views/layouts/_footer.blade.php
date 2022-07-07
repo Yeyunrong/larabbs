2088213f2d4893fade6918096abc270e7670b129
YeYunRong <3148113255@qq.com>
2022-07-07 14:10:51 +0800

    重置基础页面结构

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="footer">
   <div class=container>
-    <p class="float-start">
-      由 <a href="#">yeyunrong</a> 设计和编码<span style=“color: #e27575;font-size: 14px”>💗</span>
+    <p class="float-left">
+      由 <a href="#" target="_blank">yeyunrong</a> 设计和编码<span style=“color: #e27575; font-size: 14px;”>💗</span>
     </p>
 
-    <p class="float-end"><a href="#">联系作者</a></p>
+    <p class="float-right"><a href="#">联系作者</a></p>
   </div>
 </footer>
--- resources/views/shared/_message.blade.php
+++
@@ -1,9 +0,0 @@
-@foreach (['danger', 'awrning', 'success', 'info'] as $item)
-    @if (session()->has($msg))
-        <div class="flash-message">
-          <p class="alert alert-{{ $msg }}">
-            {{ session()->get($msg) }}
-          </p>
-        </div>
-    @endif
-@endforeach
