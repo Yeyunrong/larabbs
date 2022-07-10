229d667a0ef14a8a3bc45feb3c757fcb827c59a5
YeYunRong <3148113255@qq.com>
2022-07-10 15:33:50 +0800

    强制用户认证

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,6 +9,7 @@ class PagesController extends Controller
     //返回基础视图-首页
     public function root()
     {
+        //dd(\Auth::user()->hasVerifiedEmail());
         return view('pages.root');
     }
 }
