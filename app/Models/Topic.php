377686e8547ebf7c04ea801cd283e776b7eb16b8
YeYunRong <3148113255@qq.com>
2022-07-16 06:55:36 +0800

    data:话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -12,4 +12,20 @@ class Topic extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * 获取对应的分类
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * 获取对应的作者
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ resources/views/topics/_topic_list.blade.php
@@ -0,0 +1,55 @@
+@if (count($topics))
+  <ul class="list-unstyled">
+    @foreach ($topics as $topic)
+      <li class="media">
+
+        {{-- 用户头像和名称 --}}
+        <div class="media-left">
+          <a href="{{ route('users.show', [$topic->user_id]) }}">
+            <img class="media-object img-thumbnail mr-3" style="width:52px; height:52px;" src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}">
+          </a>
+        </div>
+
+        {{-- 话题内容 --}}
+        <div class="media-body">
+          <div class="media-heading mt-0 mt-1">
+            <a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
+              {{ $topic->title }}
+            </a>
+            <a class="float-right" href="{{ route('topics.show', [$topic->id]) }}">
+              <span class="badge badge-secondary badge-pill">
+                {{ $topic->reply_count }}
+              </span>
+            </a>
+          </div>
+
+          {{-- 分类名称 --}}
+          <small class="media-body meta text-secondary">
+            <a class="text-secondary" href="#" title="{{ $topic->category->name }}">
+              <i class="far fa-user"></i>
+              {{ $topic->category->name }}
+            </a>
+
+            <span> • </span>
+            <a class="text-secondary" href="{{ route('users.show',[$topic->user_id]) }}" title="{{ $topic->user->name }}">
+              <i class="far fa-user"></i>
+              {{ $topic->user->name }}
+            </a>
+
+            <span> • </span>
+            <i class="far fa-user"></i>
+            <span class="timego" title="最后活跃于:{{ $topic->update_at }}">{{ $topic->updated_at->diffForHumans() }}</span>
+          </small>
+        </div>
+      </li>
+
+      @if (! $loop->last)
+          <hr>
+      @endif
+
+    @endforeach
+  </ul>
+
+@else
+  <div class="empty-block">暂无数据 ～_～ </div>
+@endif
